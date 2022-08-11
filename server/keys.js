aa897784455c5a2d24adfdf6dc619b52ec3887f4
Rahul Roshan G <103802956+RAHUL-ROSHAN-G@users.noreply.github.com>
2022-08-11 17:35:25 +0530

    Update keys.js

--- server/keys.js
+++ server/keys.js
@@ -1,4 +1,4 @@
 module.exports={
-	MONGOURI:"mongodb+srv://rahulroshang:WCwLpe37ne1sEiY1@cluster0.93vm8.mongodb.net/?retryWrites=true&w=majority",
-	JWT_SECRET: "mnbvcxz"
+	MONGOURI:"Mongodb uri here",
+	JWT_SECRET: "jwt secret key goes here"
 }
